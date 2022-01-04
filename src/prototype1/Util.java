4ff203b3efbd11cd94327c35f9dec5de9e06caaf
caelunshun <caelunshun@gmail.com>
2022-01-03 20:04:36 -0700

    Add Fisher-Yates shuffle

--- src/prototype1/Util.java
+++ src/prototype1/Util.java
@@ -2,6 +2,8 @@ package prototype1;
 
 import battlecode.common.Direction;
 
+import java.util.Random;
+
 /**
  * Assorted utilities.
  */
@@ -16,4 +18,18 @@ public class Util {
             Direction.WEST,
             Direction.NORTHWEST,
     };
+
+    private final static Random rng = new Random(63125);
+
+    /**
+     * The Fisher-Yates shuffle
+     */
+    public static <T> void shuffle(T[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int j = rng.nextInt(array.length);
+            T temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
 }
