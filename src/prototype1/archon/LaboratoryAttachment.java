78cd97f94854bcb8610ed723e2340c80818b9478
Student_Zachary_Klein_2023 <zachary78922@gmail.com>
2022-01-03 19:23:57 -0700

    basic laboratory code

---
+++ src/prototype1/archon/LaboratoryAttachment.java
@@ -0,0 +1,28 @@
+package prototype1.archon;
+
+import battlecode.common.GameActionException;
+import prototype1.Attachment;
+import prototype1.Robot;
+
+public class LaboratoryAttachment extends Attachment {
+    public LaboratoryAttachment(Robot robot) {
+        super(robot);
+    }
+
+    @Override
+    public void doTurn() throws GameActionException {
+        createGold();
+    }
+    public void createGold() throws GameActionException{
+        if(rc.canTransmute() && moreGold()){
+            rc.transmute();
+        }
+    }
+    public boolean moreGold() throws GameActionException{
+        //Probably change this eventually once we understand more
+        if(rc.getTeamLeadAmount(rc.getTeam()) < 1000){
+            return false;
+        }
+        return true;
+    }
+}
