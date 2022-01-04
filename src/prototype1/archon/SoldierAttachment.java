23ca885b7a6c2ad2d67ba4405afbf1ae363dde33
Student_Zachary_Klein_2023 <zachary78922@gmail.com>
2022-01-03 20:03:48 -0700

    movement

--- src/prototype1/archon/SoldierAttachment.java
+++ src/prototype1/archon/SoldierAttachment.java
@@ -3,19 +3,27 @@ package prototype1.archon;
 import battlecode.common.*;
 import prototype1.Attachment;
 import prototype1.Robot;
+import prototype1.Util;
+import prototype1.nav.Navigator;
 
 public class SoldierAttachment extends Attachment {
+    private final Navigator nav;
     public SoldierAttachment(Robot robot) {
         super(robot);
+        nav = new Navigator(robot);
     }
 
     @Override
     public void doTurn() throws GameActionException {
-        lookForEnemy();
-    }
-    public void lookForEnemy() throws GameActionException{
-        if(!rc.isActionReady()){
+        if(lookForEnemy()){
             return;
+        }
+        moveRandom();
+
+    }
+    public boolean lookForEnemy() throws GameActionException{
+        if(!rc.isActionReady()){
+            return false;
         }
         MapLocation[] locations = rc.getAllLocationsWithinRadiusSquared(rc.getLocation(), rc.getType().visionRadiusSquared);
         //looks for archons first
@@ -25,6 +33,7 @@ public class SoldierAttachment extends Attachment {
                 if(rc.senseRobotAtLocation(location).getType() == RobotType.ARCHON && rc.senseRobotAtLocation(location).getTeam() != rc.getTeam()){
                     if(rc.canAttack(location)){
                         rc.attack(location);
+                        return true;
                     }
                 }
             }
@@ -34,10 +43,20 @@ public class SoldierAttachment extends Attachment {
                 if(rc.senseRobotAtLocation(location).getTeam() != rc.getTeam()){
                     if(rc.canAttack(location)){
                         rc.attack(location);
-                        break;
+                        return true;
                     }
                 }
             }
+        }
+        return false;
+    }
+    public void moveRandom() throws GameActionException{
+        for(Direction dir : Util.DIRECTIONS) {
+            if(rc.canMove(dir)){
+                rc.move(dir);
+                break;
+            }
+
         }
     }
 }
