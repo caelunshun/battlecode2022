69bd042543197ca6e85932fbecae830813c6d0c1
Student_Zachary_Klein_2023 <zachary78922@gmail.com>
2022-01-04 17:29:24 -0700

    just commititng before pulling

---
+++ src/prototype1/sage/SageAttachment.java
@@ -0,0 +1,17 @@
+package prototype1.watchtower;
+
+import battlecode.common.*;
+import prototype1.Attachment;
+import prototype1.Robot;
+import prototype1.Util;
+import prototype1.nav.Navigator;
+
+public class SageAttachment extends Attachment {
+    public SageAttachment(Robot robot) {
+        super(robot);
+    }
+    @Override
+    public void doTurn() throws GameActionException {
+    }
+
+}
